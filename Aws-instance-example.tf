c1ce1276d556bd7a805fd508e1970fe95a1ea770
to.patelvijay@gmail.com <to.patelvijay@gmail.com>
2019-06-30 18:41:54 +0530

    added terraform variable files and modified main source file

--- Aws-instance-example.tf
+++ Aws-instance-example.tf
@@ -1,16 +1,11 @@
-provider "aws"{
- region     = "us-east-1"
- access_key = "AKIASGHX7SHCBQNPCDUB"
- secret_key = "apmH6dOPA1GR7t9/5QQlyhZ6vdI6MFP6FjpEcJ2L"
- version = "~> 2.0"
-
+provider "aws" {
+  region     = "${var.region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  version    = "~> 2.0"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0ad82a384c06c911e"
+resource "aws_instance" "web1" {
+  ami           = "${lookup(var.ami_id, var.region)}"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
