fcfd4471fcfe356f609eb5ba8582a525d641ebea
Vijay Patel <35554905+Patelvijaykumar@users.noreply.github.com>
2019-10-03 18:54:56 +0530

    Update apache_config.sh

--- terraform-aws-ec2-userdata/apache_config.sh
+++ terraform-aws-ec2-userdata/apache_config.sh
@@ -3,4 +3,4 @@ sudo yum update -y
 sudo yum install -y httpd.x86_64
 sudo service httpd start
 sudo service httpd enable
-echo "<h1>Deployed via Terraform</h1>" | yum tee /var/www/html/index.html
+echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
