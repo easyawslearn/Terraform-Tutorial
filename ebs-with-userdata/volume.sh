7fdc2db335897705a9a76746bd9660c1f4d635de
vijay patel <to.patelvijay@gmail.com>
2019-10-21 16:22:39 +0530

    bug fix

--- ebs-with-userdata/volume.sh
+++ ebs-with-userdata/volume.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 sleep 30
@@ -6,12 +5,3 @@ sudo mkdir -p  /data
 sleep 30
 sudo mkfs.ext4 ${device_name}
 sudo mount ${device_name} /data
-=======
-#!/bin/bash -xe
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-sleep 30
-sudo mkdir -p  /data
-sleep 30
-sudo mkfs.ext4 /dev/xvdh
-sudo mount /dev/xvdh /data
->>>>>>> 59a40dfafde6bc01cab380fc5f663dc7852dee9a
