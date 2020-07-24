469c6d30b8a0f581bc141fdf0bdbafecf24c3797
Patel, VijayKumar <to.patelvijay@gmail.com>
2020-07-24 12:12:20 +0530

    new project added

--- Devops-project1/output.tf
+++ Devops-project1/output.tf
@@ -1,6 +1,3 @@
 output "public_ip" {
   value = "${aws_instance.user_data_example.public_ip}"
 }
-output "user_data_example_input_file" {
-  value = "${aws_instance.user_data_example_input_file.public_ip}"
-}
