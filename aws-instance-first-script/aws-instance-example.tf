281f9b841bd35df48dbb00aff4608b1bb9aa3ea5
ASLAMM123 <68051841+ASLAMM123@users.noreply.github.com>
2021-03-24 21:18:51 +0000

    Commit tag5
    Commit tag5

--- aws-instance-first-script/aws-instance-example.tf
+++ aws-instance-first-script/aws-instance-example.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "web1" {
    ami           = "${lookup(var.ami_id, var.region)}"
    instance_type = "t2.micro"
    tags  = {
-      Name = "new EC2 instance"
+      Key : "Name",
+      Value : "New EC2 Inst"
    }
  } 
