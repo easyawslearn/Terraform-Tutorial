b5e18393137f56fd64e2bed02eb04c97c2438252
jkdave82 <jigar.k.dave@gmail.com>
2021-12-21 13:43:39 -0500

    Update variables.tf

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -1,13 +1,11 @@
 variable "region" {
-  default = "us-east-1"
+  default = "us-east-2"
 }
 
 variable "ami_id" {
   type = "map"
 
   default = {
-    us-east-1    = "ami-035b3c7efe6d061d5"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
-    eu-central-1 = "ami-9787h5h6nsn"
-  }
+    us-east-1    = "ami-0b9064170e32bde34"
+      }
 }
