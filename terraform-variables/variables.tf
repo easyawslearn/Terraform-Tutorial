7b3f8ff902ada3e9b989ad47097bc6503fc40374
jkdave82 <jigar.k.dave@gmail.com>
2021-12-21 12:03:24 -0500

    Update variables.tf

--- terraform-variables/variables.tf
+++ terraform-variables/variables.tf
@@ -10,7 +10,7 @@ default="t2.micro"
 variable "ami_id" {
 type = "map"
 default = {
-us-east-1 = "ami-035b3c7efe6d061d5"
+us-east-2 = "ami-0b9064170e32bde34"
 eu-west-2= "ami-132b3c7efe6sdfdsfd"
 eu-central-1="ami-9787h5h6nsn"
 }
