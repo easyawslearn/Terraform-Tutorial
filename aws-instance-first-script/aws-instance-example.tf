fc5872543543896810cddfe0fb07e554a0d6ed7b
Sunil <sunilbennur25@gmail.com>
2023-02-12 10:12:09 +0530

    Update aws-instance-example.tf

--- aws-instance-first-script/aws-instance-example.tf
+++ aws-instance-first-script/aws-instance-example.tf
@@ -1,4 +1,25 @@
-resource "aws_instance" "web1" {
-   ami           = "${lookup(var.ami_id, var.region)}"
-   instance_type = "t2.micro"
- }
+resource "aws_instance" "my_vm" {
+  instance_type = "t2.micro"
+  ami           = "ami-0aa7d40eeae50c9a9" //Ubuntu AMI
+
+  tags = {
+    Name = "ubuntu_server"
+  }
+}
+
+resource "aws_vpc" "my_first_vpc" {
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    name = "production_vpc"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.my_first_vpc.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    name = "prod-subnet"
+  }
+}
