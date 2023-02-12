9c156535abdc9ffca88da096f061d6cd4015a72e
Sunil <sunilbennur25@gmail.com>
2023-02-12 14:11:27 +0530

    Update variables.tf

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -7,7 +7,7 @@ variable "ami_id" {
   type = map(string)
 
   default = {
-    us-east-1    = "ami-035b3c7efe6d061d5"
+    us-east-1    = "ami-0557a15b87f6559cf"
     eu-west-2    = "ami-132b3c7efe6sdfdsfd"
     eu-central-1 = "ami-9787h5h6nsn"
   }
