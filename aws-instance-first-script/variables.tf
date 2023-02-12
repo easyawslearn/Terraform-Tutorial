8afc764ba4140f44893ea33f04b521191eac1457
Sunil <sunilbennur25@gmail.com>
2023-02-12 09:59:11 +0530

    update ami id

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -6,8 +6,6 @@ variable "ami_id" {
   type = "map"
 
   default = {
-    us-east-1    = "ami-035b3c7efe6d061d5"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
-    eu-central-1 = "ami-9787h5h6nsn"
+    us-east-1    = "ami-0aa7d40eeae50c9a9"
   }
 }
